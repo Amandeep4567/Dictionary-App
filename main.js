86f54968546076251ffb841bb9fe745f863338d5
Amandeep Kalakar <amankumaradk@gmail.com>
2023-02-09 13:26:01 +0530

    updated

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ btn.addEventListener("click", () => {
     data[0].meanings[0].definitions[0].example || ""
   }</p>
 </div>`;
-      sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);
+      sound.setAttribute("src", `${data[0].phonetics[0].audio}`);
     })
     .catch(() => {
       result.innerHTML = `<h3>Couldn't Find The Work`;
